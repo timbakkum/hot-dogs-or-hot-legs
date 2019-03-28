a821c14f45137e96fc928f03afec4862d4e8bf8b
Tim Bakkum <timbakkum@gmail.com>
2019-03-28 10:41:40 +0100

    fb setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { message } from "antd";
+import { FirebaseDatabaseProvider } from "@react-firebase/database";
 import { UploadZone } from "./UploadZone";
 import { ResultZone } from "./ResultZone";
 import "antd/dist/antd.css";
@@ -8,43 +9,45 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <header className="app__header">
-          <h1>
-            <span role="img" aria-label="hotdog">
-              🌭
-            </span>{" "}
-            or{" "}
-            <span role="img" aria-label="hot legs">
-              🦵🦵
-            </span>
-          </h1>
-        </header>
-        <main className="app__main">
-          <UploadZone
-            className="app__upload-zone"
-            props={{
-              name: "file",
-              multiple: false,
-              action: "//jsonplaceholder.typicode.com/posts/",
-              onChange(info) {
-                const status = info.file.status;
-                if (status !== "uploading") {
-                  console.log(info.file, info.fileList);
+      <FirebaseDatabaseProvider>
+        <div className="app">
+          <header className="app__header">
+            <h1>
+              <span role="img" aria-label="hotdog">
+                🌭
+              </span>{" "}
+              or{" "}
+              <span role="img" aria-label="hot legs">
+                🦵🦵
+              </span>
+            </h1>
+          </header>
+          <main className="app__main">
+            <UploadZone
+              className="app__upload-zone"
+              props={{
+                name: "file",
+                multiple: false,
+                action: "//jsonplaceholder.typicode.com/posts/",
+                onChange(info) {
+                  const status = info.file.status;
+                  if (status !== "uploading") {
+                    console.log(info.file, info.fileList);
+                  }
+                  if (status === "done") {
+                    message.success(
+                      `${info.file.name} file uploaded successfully.`
+                    );
+                  } else if (status === "error") {
+                    message.error(`${info.file.name} file upload failed.`);
+                  }
                 }
-                if (status === "done") {
-                  message.success(
-                    `${info.file.name} file uploaded successfully.`
-                  );
-                } else if (status === "error") {
-                  message.error(`${info.file.name} file upload failed.`);
-                }
-              }
-            }}
-          />
-          <ResultZone className="app__result-zone" />
-        </main>
-      </div>
+              }}
+            />
+            <ResultZone className="app__result-zone" />
+          </main>
+        </div>
+      </FirebaseDatabaseProvider>
     );
   }
 }
---
+++ src/firebase-config.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: "AIzaSyDRHDbBJmvlpgiBrxVFcZjJu4aonsMDRTE",
+  authDomain: "hotdogs-or-nice-legs.firebaseapp.com",
+  databaseURL: "https://hotdogs-or-nice-legs.firebaseio.com",
+  projectId: "hotdogs-or-nice-legs",
+  storageBucket: "hotdogs-or-nice-legs.appspot.com",
+  messagingSenderId: "624070545796"
+};
